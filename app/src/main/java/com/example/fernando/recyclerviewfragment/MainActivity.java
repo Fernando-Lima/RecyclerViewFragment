01171ade1b0032a99cf928184e6245281260a362
Fernando <fernandosupweb@gmail.com>
2018-03-20 19:29:31 -0300

    Utilizando o FragmentManager()

--- app/src/main/java/com/example/fernando/recyclerviewfragment/MainActivity.java
+++ app/src/main/java/com/example/fernando/recyclerviewfragment/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.fernando.recyclerviewfragment;
 
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.fernando.recyclerviewfragment.Fragment.MainFragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FragmentManager fm = getSupportFragmentManager();
+
+        //Abre uma tranzação e adiciona
+        FragmentTransaction ft = fm.beginTransaction();
+        ft.add(R.id.fragment_container,new MainFragment());
+        ft.commit();
+
+        //Substitui um fragment
+        //ft.add(R.id.fragment_container,new MainFragment());
+
+        //Remove um fragment
+        //Fragment fragment = fm.findFragmentById(R.id.fragment_container);
+        //FragmentTransaction ft = fm.beginTransaction();
+        //ft.remove(fragment);
+        //ft.commit();
+
+
     }
 }
